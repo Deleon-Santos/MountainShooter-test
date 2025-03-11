54c927a0904355884830c4002f0c1da1e2463cab
Deleon <delps.santos1987@gmail.com>
2025-03-11 17:36:47 -0300

    ajuste-no-numero-de-enimigos-na-tela

--- code/Const.py
+++ code/Const.py
@@ -2,14 +2,14 @@
 import pygame
 
 C_ORANGE = (255, 128, 0)
-C_YELLOW = (255, 255, 128)
+C_YELLOW = (255, 128, 0)
 C_WHITE = (255, 255, 255)
 C_GREEN = (0, 128, 0)
 C_CYAN = (0, 128, 128)
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
-EVENT_TIMEOUT = pygame.USEREVENT + 2
+EVENT_TIMEOUT = pygame.USEREVENT + 1
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -24,13 +24,13 @@ ENTITY_SPEED = {
     'Level2Bg3': 3,
     'Level2Bg4': 4,
     'Player1': 3,
-    'Player1Shot': 1,
+    'Player1Shot': 3,
     'Player2': 3,
     'Player2Shot': 3,
     'Enemy1': 1,
     'Enemy1Shot': 5,
-    'Enemy2': 1,
-    'Enemy2Shot': 2,
+    'Enemy2': 2,
+    'Enemy2Shot': 5,
 }
 
 ENTITY_HEALTH = {
@@ -70,13 +70,13 @@ ENTITY_DAMAGE = {
     'Level2Bg3': 0,
     'Level2Bg4': 0,
     'Player1': 1,
-    'Player1Shot': 25,
+    'Player1Shot': 20,
     'Player2': 1,
     'Player2Shot': 20,
     'Enemy1': 1,
     'Enemy1Shot': 20,
     'Enemy2': 1,
-    'Enemy2Shot': 15,
+    'Enemy2Shot': 35,
 }
 
 ENTITY_SCORE = {
@@ -98,15 +98,15 @@ ENTITY_SCORE = {
     'Player2Shot': 0,
     'Enemy1': 100,
     'Enemy1Shot': 0,
-    'Enemy2': 125,
+    'Enemy2': 300,
     'Enemy2Shot': 0,
 }
 
 ENTITY_SHOT_DELAY = {
-    'Player1': 20,
-    'Player2': 15,
-    'Enemy1': 100,
-    'Enemy2': 200,
+    'Player1': 10,
+    'Player2': 10,
+    'Enemy1': 50,
+    'Enemy2': 80,
 }
 
 # M
@@ -132,8 +132,8 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
 SPAWN_TIME = 4000
 
 # T
-TIMEOUT_STEP = 100  # 100ms
-TIMEOUT_LEVEL = 20000  # 20s
+TIMEOUT_STEP = 1200  # 100ms
+TIMEOUT_LEVEL = 120000  # 20s
 # W
 WIN_WIDTH = 576
 WIN_HEIGHT = 324
--- code/Enemy.py
+++ code/Enemy.py
@@ -15,6 +15,6 @@ class Enemy(Entity):
 
     def shoot(self):
         self.shot_delay -= 1
-        if self.shot_delay == 0:
+        if self.shot_delay == 1:
             self.shot_delay = ENTITY_SHOT_DELAY[self.name]
             return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
